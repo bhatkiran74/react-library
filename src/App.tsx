324dc9dd99a7c1968808aa79362ded8444a987f3
Kiran Bhat <bhatkiran74@gmail.com>
2024-03-16 02:16:51 +0530

    created navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Navbar } from "./layout/navbarAndFooter/navbar";
 
 function App() {
-  return (
-    <div className="App">
-      
-    <h1>Hello world</h1>
-    </div>
-  );
+  return <Navbar />;
 }
 
 export default App;
